44bce722e7f6bdc2d9dbeeda8335f0bd7a22898d
Shk123 <shaqeebsk1234@gmail.com>
2024-04-20 18:32:18 +0530

    correction

--- myntra.js
+++ myntra.js
@@ -25,7 +25,6 @@ const transporter = nodemailer.createTransport({
   secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
     user: "shaqeebsk1234@gmail.com",
-    pass: "uwxheqncnxmbbqhf",
   },
 });
 const CheckRole = (req, res, Role) => {
